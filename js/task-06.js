650c1d45ccc87ba65a81a1cbe102c6948faba079
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-08-04 13:40:52 +0300

    Add solution for task-06

---
+++ js/task-06.js
@@ -0,0 +1,10 @@
+const validationInput = document.querySelector('#validation-input');
+
+const ValidInputEl = elem => {
+  if (Number(validationInput.dataset.length) === elem.currentTarget.value.length) {
+    validationInput.classList.remove('invalid');
+    validationInput.classList.add('valid');
+  } else validationInput.classList.add('invalid');
+};
+
+validationInput.addEventListener('blur', ValidInputEl);
