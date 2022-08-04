8d340ee6d2e929fd38575842a1d1b9ae03497010
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-08-04 13:11:04 +0300

    Make changes to task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,12 +1,12 @@
 const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
 
-const allIngredientsEl = document.querySelector('#ingredients');
+const ingredientsEl = document.querySelector('#ingredients');
 
-const addIngredientsItemEl = ingredients.map(ingredient => {
+const addIngredientsItem = ingredients.map(ingredient => {
   const ingredientItem = document.createElement('li');
   ingredientItem.textContent = ingredient;
   ingredientItem.classList.add('item');
   return ingredientItem;
 });
 
-allIngredientsEl.append(...addIngredientsItemEl);
+ingredientsEl.append(...addIngredientsItem);
