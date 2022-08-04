5cfc494a4cc406f632d584bf3abebcc5bd6c7342
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-08-04 17:10:35 +0300

    Add solution for task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const numberInput = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  const addBoxesArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    div.style.width = `${30 + i * 10}px`;
+    div.style.height = `${30 + i * 10}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.marginTop = '5px';
+    addBoxesArray.push(div);
+  }
+
+  boxes.append(...addBoxesArray);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => createBoxes(numberInput.value));
+
+destroyBtn.addEventListener('click', destroyBoxes);
