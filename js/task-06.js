4783b6647b32c9f35118fa27dda9cf5d85154fff
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-08-05 19:14:12 +0300

    Make final changes for task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,7 +4,10 @@ const ValidInputEl = elem => {
   if (Number(validationInput.dataset.length) === elem.currentTarget.value.length) {
     validationInput.classList.remove('invalid');
     validationInput.classList.add('valid');
-  } else validationInput.classList.add('invalid');
+  } else {
+    validationInput.classList.add('invalid');
+    validationInput.classList.remove('valid');
+  }
 };
 
 validationInput.addEventListener('blur', ValidInputEl);
